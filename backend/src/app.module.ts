fee9dc576bd75a769504dc63efe1785d55462f30
MT-jlem <75174960+MT-jlem@users.noreply.github.com>
2024-02-12 23:02:17 +0100

    made the project run on mac

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,10 @@ import { UsersModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { ChatModule } from './chat/chat.module';
-import { ChatNoSpecGateway } from './chat--no-spec/chat--no-spec.gateway';
 
 @Module({
   imports: [UsersModule, AuthModule, ChatModule],
   controllers: [AppController],
-  providers: [GameGateway, ChatNoSpecGateway],
+  providers: [GameGateway],
 })
 export class AppModule {}
---
+++ f.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+osascript <<EOF
+tell application "iTerm"
+    tell current window
+        create tab with default profile
+        tell current session of current tab
+            write text "cd frontend && npm i && npm run dev"
+        end tell
+    end tell
+end tell
+EOF

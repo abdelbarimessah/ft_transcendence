065f33fa75125c41c32e13986a08e4727b730550
MT-jlem <75174960+MT-jlem@users.noreply.github.com>
2024-03-01 14:30:17 +0100

    add chat menu

--- frontend/src/app/(home)/chat/page.tsx
+++ frontend/src/app/(home)/chat/page.tsx
@@ -1,27 +1,29 @@
-'use client'
-import { SocketContext } from '@/app/SocketContext';
-import { useContext } from 'react';
-import LeftSide from '@/components/chat/leftSide';
-import RightSide from '@/components/chat/rightSide';
+"use client";
+import { SocketContext } from "@/app/SocketContext";
+import { useContext } from "react";
+import LeftSide from "@/components/chat/leftSide";
+import RightSide from "@/components/chat/rightSide";
+import ChatMenu from "@/components/chat/ChatMenu";
 
 function Chat() {
-    return (
-        <>
-        <div className='flex justify-start chat-bp:justify-center items-center w-screen h-screen overflow-hidden '>
+  return (
+    <>
+      <div className="flex justify-start chat-bp:justify-center items-center w-screen h-screen overflow-hidden ">
+        <div className="flex justify-start chat-bp:justify-center items-center w-[1731px] h-[1080px] bg-[#ffbb3b] ">
+          <div className=" bg-[#FFE0B3] min-w-[340px] max-w-[460px] h-full w-full rounded-[29px_0px_0px_29px]">
+            <LeftSide />
+          </div>
 
-          <div className="flex justify-start chat-bp:justify-center items-center w-[1731px] h-[1080px] bg-[#ffbb3b] ">
-
-              <div className=" bg-[#FFE0B3] min-w-[340px] max-w-[460px] h-full w-full rounded-[29px_0px_0px_29px]">
-                <LeftSide />
-              </div>
-
-              <div className="bg-[#FFF0D2]  min-w-[415px] max-w-[1271px] h-full w-full rounded-[0px_29px_29px_0px]">
-                <RightSide />
-              </div>
-            </div>
+          <div className="bg-[#FFF0D2]  min-w-[415px] max-w-[1271px] h-full w-full rounded-[0px_29px_29px_0px]">
+            <RightSide />
+          </div>
+          <div className="h-full">
+            <ChatMenu></ChatMenu>
+          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default Chat;
